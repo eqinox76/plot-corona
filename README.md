c048f2ccc6db62b8264bdefc0fb70db5863b5b0a
eqinox76 <5861557+eqinox76@users.noreply.github.com>
2020-03-18 10:04:08 +0100

    feat: made a few countries visible

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # plot-corona
-Hobby project to explore Wikidata and graph plotting by example of the corona virus outbreak 2020
+Hobby project to explore Wikidata and graph plotting by example of the corona virus outbreak 2020.
+The available data on Wikidata is surprisingly sparse.
+
+# usage
+You will need to add a `user-config.py` for the pywikibot. Example content:
+```
+mylang = 'wikidata'
+family = 'wikidata'
+usernames['wikidata']['wikidata'] = u'MyUserNameWhichDoesNotExist'
+``` 
+
+
--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 from datetime import date
-from pprint import pprint
 from typing import List, Tuple
 
+import plotly.graph_objects as go
 import pywikibot
 
-# https://www.wikidata.org/wiki/Wikidata:Pywikibot_-_Python_3_Tutorial/Setting_up_Shop
+# pywikibot.config.verbose_output = 1
 site = pywikibot.Site("wikidata", "wikidata")
 repo = site.data_repository()
 
 
 def main():
-    pprint(extract("Q84055514"))
+    data = {
+        "india": extract("Q84055514"),
+        "italy": extract("Q84104992"),
+        "germany": extract("Q83889294"),
+        "us": extract("Q83873577"),
+        "russia": extract("Q84098939"),
+        "spain": extract("Q84166704"),
+        "japan": extract("Q83872291")
+    }
+
+    fig = go.Figure(
+        data=[
+            go.Scatter(x=[i[0] for i in data[country]],
+                       y=[i[1] for i in data[country]],
+                       mode='lines+markers',
+                       name=country)
+            for country in data
+        ],
+        layout=go.Layout(
+            title=go.layout.Title(text="plot-corona"),
+            yaxis_type="log"
+        )
+    )
+    fig.show()
 
 
 def extract(page: str) -> List[Tuple[date, int]]:
@@ -20,7 +43,7 @@ def extract(page: str) -> List[Tuple[date, int]]:
             to_date(e.qualifiers['P585'][0].getTarget()),
             int(e.getTarget().amount),
         )
-        for e in item.get()['claims']['P1603']
+        for e in item.get()['claims']['P1603'] if 'P585' in e.qualifiers
     ],
         key=lambda a: a[0])
 
